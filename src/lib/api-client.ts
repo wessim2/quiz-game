ae053d40f318dc62ab2490423f9e08712dc512f8
Meddeb Wessim <wessim-ess@live.fr>
2024-08-02 18:58:43 +0100

    edit api-client

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
-const url : string = import.meta.env.VITE_TRIVIA_API
-
+// const url : string = import.meta.env.VITE_TRIVIA_API
+const url : string = "https://opentdb.com"
 export const api = Axios.create({
   baseURL: url,
 });
